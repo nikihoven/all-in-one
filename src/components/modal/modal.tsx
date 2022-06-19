47caabe75c413ae836b37b82c3165ae0cc68e089
Nikita <nikki.xovenetti@mail.ru>
2022-06-19 15:55:03 +0300

    Implemented react portal feature in 'modal' component

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,9 @@
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect, useMemo } from 'react'
+import { createPortal } from 'react-dom'
 
 import './modal.scss'
+
+const modalRoot = document.getElementById('modals')!
 
 interface ModalProp {
     children: ReactNode
@@ -8,13 +11,23 @@ interface ModalProp {
 }
 
 const Modal: FC<ModalProp> = ({children, onClose}) => {
+    const element = useMemo(() => document.createElement('div'), [])
 
-    return (
+    useEffect(() => {
+        modalRoot.appendChild(element)
+
+        return () => {
+            modalRoot.removeChild(element)
+        }
+    }, [])
+
+    return createPortal(
         <div className="modal" onClick={onClose}>
             <div className="modal__card" onClick={e => e.stopPropagation()}>
                 {children}
             </div>
-        </div>
+        </div>,
+        modalRoot
     )
 }
 
