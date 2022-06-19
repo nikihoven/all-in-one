6ef0da6c09c25a4f772895b7ad01ffeb9bba00a4
Nikita <nikki.xovenetti@mail.ru>
2022-06-19 16:00:07 +0300

    Added logic for creating and deleting 'Modal' component in Store

--- src/store/models/index.ts
+++ src/store/models/index.ts
@@ -1,11 +1,14 @@
 import { initialTodoModel, TodoModel } from './todo.model'
+import { initialModalModel, ModalModel } from './modal.model'
 
 export interface Model {
-    todo: TodoModel
+    todo: TodoModel,
+    modal: ModalModel
 }
 
 const model: Model = {
-    todo: initialTodoModel
+    todo: initialTodoModel,
+    modal: initialModalModel
 }
 
 export { model }
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,13 @@
+import { ReactNode } from 'react'
+
 export type Todo = {
     id: string,
     text: string,
     completed: boolean,
     creationDate: Date
+}
+
+export type Modal = {
+    id: string,
+    node: ReactNode
 }
